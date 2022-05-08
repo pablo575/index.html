09f01d382b4633d5d7817216f7785e947debfd0c
pablo575 <96500342+pablo575@users.noreply.github.com>
2022-05-08 09:58:51 -0500

    nuevo comit

---
+++ archi.js/js.js
@@ -0,0 +1,18 @@
+
+// let sola;
+// sola ="todos son marikas menos yo";
+// alert (sola);
+// let mai;
+
+//*let jua;
+// let isa;
+// mai ="julia";
+// jua ="julia campestre";
+// isa ="maicol";
+// alert(mai)
+// alert(jua)
+// alert(isa)*// 
+let x=500 ;
+let y=600;
+let z =x+y;
+console.log(z)
