9d73a68dc265f1536f6239e2e788c22a9561b5dd
pablo <maicolysofia55@gmail.com>
2023-05-21 12:40:33 -0500

    version_tres

---
+++ login/js2.js
@@ -0,0 +1,11 @@
+function login () {
+    let user, password;
+    user = document.getElementById("usuario"). Value;
+    password = document.getElementById("contraseña"). Value;
+
+    if (user == "prueda" && password == "1234"){
+        alert =("inisiaste secio.");
+    } else {
+            alert =("no tienes acceso.");
+        }
+}
